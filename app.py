c9be1d090925d0a5e584d49022b947265883b1f9
Uarsa <almirpython@gmail.com>
2020-06-09 21:01:02 +0300

    Add new url (/create_article), new class Article, DB.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    device = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    tel = db.Column(db.Integer)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -16,9 +32,16 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/create_article')
+def create_article():
+    return render_template('create_article.html')
+
+
+'''
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return f"User: {name}, id: {id}."
+'''
 
 
 if __name__ == '__main__':
