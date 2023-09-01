a99fefa27571a04b1a3b7c514c685ad40d8ad798
Uarsa <almirpython@gmail.com>
2023-09-01 16:02:25 +0300

    add few columns in article

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clients.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -12,12 +13,17 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     tel = db.Column(db.String(20))
+    type = db.Column(db.String(10))
     device = db.Column(db.String(100))
     description = db.Column(db.Text)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    part_price = db.Column(db.Integer())
+    total_price = db.Column(db.Integer())
+    income = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Article %r>' % self.id
+
 
 '''
 @app.route('/')
@@ -29,6 +35,7 @@ def index():
     arts = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', arts=arts)
 '''
+
 
 @app.route('/about')
 def about():
@@ -43,8 +50,7 @@ def posts():
     # get all elements
     # articles = Article.query.all()
     # get all elements sorted by column
-    
-    
+
     if request.method == "POST":
         find = request.form['find']
         find = find.lower()
@@ -53,7 +59,7 @@ def posts():
 
         texts = []
         for el in articles:
-            s = "{} {} {} {}".format(el.name, el.tel, el.device, el.description).lower()
+            s = "{} {} {} {} {} {} {} {}".format(el.name, el.tel, el.type, el.device, el.description, el.part_price, el.total_price, el.income).lower()
             texts.append(s)
     
         matches = []
@@ -71,6 +77,7 @@ def posts():
         
         arts = Article.query.order_by(Article.date.desc()).all()
         return render_template('posts.html', arts=arts)
+
 
 '''
 #test area
@@ -110,6 +117,7 @@ def find():
 #test area
 '''
 
+
 @app.route('/posts/<int:id>')
 def post_detail(id):
     article = Article.query.get(id)
@@ -133,13 +141,23 @@ def create_article():
     if request.method == "POST":
         name = request.form['name']
         tel = request.form['tel']
+        type = request.form['type']
         device = request.form['device']
         description = request.form['description']
+        part_price = request.form['part_price']
+        total_price = request.form['total_price']
+        income = int(total_price) - int(part_price)
+        # print(type(part_price))
+        # print(type(total_price))
 
         article = Article(name=name,
                           tel=tel,
+                          type=type,
                           device=device,
-                          description=description
+                          description=description,
+                          part_price=part_price,
+                          total_price=total_price,
+                          income=income
                           )
 
         db.session.add(article)
@@ -158,8 +176,12 @@ def post_update(id):
     if request.method == "POST":
         article.name = request.form['name']
         article.tel = request.form['tel']
+        article.type = request.form['type']
         article.device = request.form['device']
         article.description = request.form['description']
+        article.part_price = request.form['part_price']
+        article.total_price = request.form['total_price']
+        # article.total_price = request.form['income']
 
         try:
             db.session.commit()
